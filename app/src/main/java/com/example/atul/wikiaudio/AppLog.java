c2908cde0cc8b8e615c595085ef1f63bd192b66f
Atul22 <atuliitrpr@gmail.com>
2016-08-20 21:17:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/atul/wikiaudio/AppLog.java
@@ -0,0 +1,15 @@
+package com.example.atul.wikiaudio;
+
+import android.util.Log;
+
+/**
+ * Created by atul on 8/7/16.
+ */
+public class AppLog {
+
+    private static final String APP_TAG = "AudioRecorder";
+
+    public static int logString(String message) {
+        return Log.i(APP_TAG, message);
+    }
+}
