d4b89fd4f337fa3f38803439349dce71b637d506
mani-shailesh <shailesh.pandey@iitrpr.ac.in>
2017-03-15 02:44:01 +0530

    Add wiktionary logo, make API call for search

--- app/src/main/java/com/example/atul/wikiaudio/activity/SearchActivity.java
+++ app/src/main/java/com/example/atul/wikiaudio/activity/SearchActivity.java
@@ -1,5 +1,6 @@
 package com.example.atul.wikiaudio.activity;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -8,16 +9,48 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.example.atul.wikiaudio.R;
+import com.example.atul.wikiaudio.rest.MediaWikiClient;
 import com.example.atul.wikiaudio.rest.ServiceGenerator;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class SearchActivity extends AppCompatActivity {
+
+    private SearchView searchBar;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        searchBar = (SearchView) findViewById(R.id.search_bar);
+        searchBar.requestFocus();
+        searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                search(s);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
     }
 
     @Override
@@ -55,6 +88,56 @@ public class SearchActivity extends AppCompatActivity {
                 LoginActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private void search(String query) {
+        MediaWikiClient mediaWikiClient = ServiceGenerator.createService(MediaWikiClient.class,
+                getApplicationContext());
+        Call<ResponseBody> call = mediaWikiClient.search("query", "search", query, true);
+
+        progressDialog = ProgressDialog.show(this, "Search", "Fetching results...", true);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    try {
+                        String responseStr = response.body().string();
+                        try {
+                            processSearchResult(responseStr);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            searchFailed("Server misbehaved! Please try again later.");
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        searchFailed("Please check your connection!");
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                searchFailed("Please check your connection!");
+            }
+        });
+    }
+
+    private void searchFailed(String msg) {
+        if (progressDialog != null)
+            progressDialog.dismiss();
+        Toast.makeText(this, "Search failed!\n" + msg, Toast.LENGTH_LONG).show();
+    }
+
+    private void processSearchResult(String responseStr) throws JSONException {
+        JSONObject reader;
+        JSONObject tokenJSONObject;
+        reader = new JSONObject(responseStr);
+
+        if (progressDialog != null)
+            progressDialog.dismiss();
+
+        ImageView wiktionaryLogo = (ImageView) findViewById(R.id.wiktionary_logo);
+        wiktionaryLogo.setVisibility(View.GONE);
     }
 }
 
